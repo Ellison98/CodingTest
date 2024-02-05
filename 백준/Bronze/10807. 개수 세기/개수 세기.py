670c03a431fec70187f6dd14bfa113079a7b8def
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-02-05 23:53:29 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,7 +1,19 @@
-n = int(input())
-numbers = list(map(int, input().split()))
-v = int(input())
+# 정수의 개수를 입력받음
+N = int(input())
 
-count = numbers.count(v)
+# 정수들을 리스트에 저장
+list_box = list(map(int, input().split()))
 
+# 찾으려는 정수를 입력받음
+search_num = int(input())
+
+# count를 0으로 초기화
+count = 0
+
+# 리스트 안에서 찾으려는 정수를 카운트함
+for i in range(N):
+    if search_num == list_box[i]:
+        count += 1
+        
+# 같은 정수의 개수를 출력
 print(count)
