1f089fb8983f6a741e3420ce1728ee63ddbe2e61
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-03-09 23:11:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = ''
+    even = True
+    
+    for letter in s:
+        if letter == ' ':
+            answer += letter
+            even = True
+            continue
+        elif even:
+            answer += letter.upper()
+        else:
+            answer += letter.lower()
+        even = not even
+    
+    return answer
