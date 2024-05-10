e57fbd5b10684698f598cc253152782a7889d4c7
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-05-10 15:44:58 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -1,9 +1,1 @@
-function solution(num_list, n) {
-    var answer = [];
-    
-    for (let i = 0; i < n; i++) {
-        answer.push(num_list[i]);
-    }
-    
-    return answer;
-}
+const solution = (num_list, n) => num_list.splice(0, n);
