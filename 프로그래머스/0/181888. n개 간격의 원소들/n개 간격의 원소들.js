73672a7a0d5e7b4c782220b62fc9d2290cc60896
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-05-10 15:37:42 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,9 +1,1 @@
-function solution(num_list, n) {
-    let result = [];
-    
-    for (let i = 0; i < num_list.length; i+=n) {
-            result.push(num_list[i]);
-    }
-    
-    return result;
-}
+const solution = (num_list, n) => num_list.filter((_, index) => index % n === 0);
