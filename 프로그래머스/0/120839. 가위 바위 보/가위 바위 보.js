316f9bb48b61c46f763100aa40ed701b24c1d78a
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-05-11 17:46:32 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,25 +1,9 @@
 function solution(rsp) {
-    
-    // 가위 바위 보 정의
-    let 가위 = 2;
-    let 바위 = 0;
-    let 보 = 5;
-    
-    // 승패 결과를 담을 새로운 배열
-    let 승리결과 = [];
-    
-    // rep의 길이 만큼 반복
-    for (let i = 0; i < rsp.length; i++) {
-        if (Number(rsp[i]) === 가위) {
-            승리결과 += 바위;
-        }
-        else if (Number(rsp[i]) === 바위) {
-            승리결과 += 보;
-        }
-        else {
-            승리결과 += 가위;
-        }
+    let arr = {
+        2 : 0,
+        0 : 5,
+        5 : 2,
     }
     
-    return 승리결과;
+    return [...rsp].map(v => arr[v]).join("");
 }
