991d3c4326c93812b542141ff77bdb11f89a0777
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-05-17 00:01:34 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,15 +1,5 @@
 function solution(seoul) {
-    var answer = '';
+    let answer = seoul.indexOf('Kim');
     
-    let wk = '';
-    
-    for (let i = 0; i < seoul.length; i++) {
-        if (seoul[i] === 'Kim') {
-            wk = i;
-        }
-    }
-    
-    answer = `김서방은 ${wk}에 있다`;
-    
-    return answer;
+    return `김서방은 ${answer}에 있다`;
 }
