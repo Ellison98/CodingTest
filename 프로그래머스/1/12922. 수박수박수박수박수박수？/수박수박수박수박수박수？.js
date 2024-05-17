f9011b22ae457e5e954c90b196ccc4e2a58c2799
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-05-18 00:59:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = '';
+    
+    for (let i = 0; i < n; i++) {
+        if (i === 0 || i % 2 === 0) {
+            answer += '수';
+        }
+        else if (i === 1 || i % 2 !== 0) {
+            answer += '박';
+        }
+    }
+    
+    return answer;
+}
