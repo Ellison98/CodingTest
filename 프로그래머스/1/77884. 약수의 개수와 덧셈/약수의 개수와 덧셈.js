fd063114a455934634557d46894d86c75c519e60
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-05-18 01:49:43 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,30 +1,11 @@
-function countDivisors(n) {
-    let count = 0;
-    for (let i = 1; i <= Math.sqrt(n); i++) {
-        if (n % i === 0) {
-            if (i === n / i) {
-                count += 1; // i와 n / i가 같은 경우 (제곱근인 경우)
-            } else {
-                count += 2; // i와 n / i가 다른 경우
-            }
-        }
-    }
-    return count;
-}
-
 function solution(left, right) {
-    let result = 0;
-    for (let num = left; num <= right; num++) {
-        const divisorsCount = countDivisors(num);
-        if (divisorsCount % 2 === 0) {
-            result += num; // 약수의 개수가 짝수인 경우 더하기
+    var answer = 0;
+    for (let i = left; i <= right; i++) {
+        if (Number.isInteger(Math.sqrt(i))) {
+            answer -= i;
         } else {
-            result -= num; // 약수의 개수가 홀수인 경우 빼기
+            answer += i;
         }
     }
-    return result;
+    return answer;
 }
-
-// 예시 사용
-console.log(solution(13, 17)); // 43
-console.log(solution(24, 27)); // 52
