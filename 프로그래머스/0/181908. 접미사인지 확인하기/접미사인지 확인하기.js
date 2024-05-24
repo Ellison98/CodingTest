d19c38357e10f51db2b49e40e9e55027800112cf
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-05-24 14:38:44 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, is_suffix) {
+    var answer = 0;
+    let mL = my_string.length;  //  6
+    let iL = is_suffix.length;  //  3
+    
+    if (mL < iL) return 0;
+    
+    return my_string.slice((mL - iL), mL) === is_suffix ? 1 : 0;
+}
