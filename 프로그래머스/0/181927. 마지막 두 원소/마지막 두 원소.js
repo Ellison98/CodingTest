5a277f1ec6cb5db3f3a52c1e66d1d80a6cc0ab80
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-05-27 10:23:42 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,14 +1,4 @@
 function solution(num_list) {
-    
-    //  last index의 크기가 그 전의 index보다 크면 ? - : last index * 2
-    let nL = num_list.length;
-    
-    if (num_list[nL - 2] < num_list[nL - 1]) {
-        num_list.push((num_list[nL - 2] - num_list[nL - 1]) * -1);
-    }
-    else {
-        num_list.push(num_list[nL - 1] * 2);
-    }
-    
-    return num_list;
+    const [a, b] = [...num_list].reverse();
+    return [...num_list, a > b ? a - b : a * 2];
 }
