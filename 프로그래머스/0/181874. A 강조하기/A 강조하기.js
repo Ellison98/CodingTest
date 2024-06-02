d5a906c63b223da8f0850bb8d92352ea2decaea5
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-06-02 11:22:40 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,13 +1,3 @@
 function solution(myString) {
-    return [...myString].map((v) => {
-        if (v === 'a') {
-            return 'A';
-        } else if (v === 'A') {
-            return 'A';
-        } else if (v >= 'A' && v <= 'Z') {
-            return v.toLowerCase();
-        } else {
-            return v;
-        }
-    }).join('');
+    return myString.toLowerCase().replaceAll('a', 'A')
 }
