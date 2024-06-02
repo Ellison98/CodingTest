e873f756a8ce74e47029d1b44e3e83c7b9ab49da
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-06-02 22:27:28 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let answer = []
+    answer.push(n)
+    
+    while(n !== 1) {
+        if (n % 2 === 0) {
+            answer.push(n / 2)
+            n = n / 2
+        }
+        else {
+            answer.push(3 * n + 1)
+            n = 3 * n + 1
+        }
+    }
+    
+    return answer
+}
