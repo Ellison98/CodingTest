4c548883efc56e50af8545b6b706e1790ef2761b
SEONGSUIM <93025344+Ellison98@users.noreply.github.com>
2024-06-03 20:46:29 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,15 +1,5 @@
 function solution(my_string, num1, num2) {
-    var answer = '';
-    
-    
-    
-    return [...my_string].map((v, idx) => {
-        if (idx === num1) {
-            return my_string[num2]
-        }
-        else if (idx === num2) {
-            return my_string[num1]
-        }
-        return v
-    }).join('')
+    my_string = my_string.split('');
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    return my_string.join('');
 }
