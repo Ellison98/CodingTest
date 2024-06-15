aa630c8ee1cf891524a1319bc72a8588058b8c8f
Larry Ellison <93025344+Ellison98@users.noreply.github.com>
2024-06-15 20:57:59 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -1,13 +1,9 @@
 function solution(my_string) {
-    let suffixes = [];
+    let answer = [];
     
-    // 모든 접미사를 생성
     for (let i = 0; i < my_string.length; i++) {
-        suffixes.push(my_string.slice(i));
+        answer.push(my_string.slice(i));
     }
     
-    // 접미사들을 사전순으로 정렬
-    suffixes.sort();
-    
-    return suffixes;
+    return answer.sort()
 }
