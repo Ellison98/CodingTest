c60d1ecd2090818296f4ef0f452e6fd5e2fcec66
Larry Ellison <93025344+Ellison98@users.noreply.github.com>
2024-06-29 01:14:32 +0900

    [level 0] Title: 문자열 돌리기, Time: 69.49 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -10,16 +10,7 @@ rl.on('line', function (line) {
     input.push(line);
 }).on('close', function () {
     const str = input[0];
-    const rows = str.split(' ');
-    const maxLen = Math.max(...rows.map(row => row.length));
-    let result = '';
-
-    for (let i = 0; i < maxLen; i++) {
-        for (let j = rows.length - 1; j >= 0; j--) {
-            result += rows[j][i] ? rows[j][i] : ' ';
-        }
-        result += '\n';
+    for (let i = 0; i < str.length; i++) {
+        console.log(str[i])
     }
-
-    console.log(result.trim());
 });
