368702096410796ac08f0fb6950cd6fd01c1007d
Larry Ellison <93025344+Ellison98@users.noreply.github.com>
2024-06-29 02:00:13 +0900

    [level 0] Title: 세로 읽기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,13 +1,3 @@
 function solution(my_string, m, c) {
-    let answer = [];
-    
-    for (let i = 0; i < my_string.length; i++) {
-        if (i === c - 1) { 
-            for (let j = i; j < my_string.length; j += m) {
-                answer.push(my_string[j])
-            }
-        }
-    }
-    
-    return answer.join('')
+    return [...my_string].filter((_, i) => i % m === c - 1).join('')
 }
