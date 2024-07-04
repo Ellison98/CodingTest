3dc4c55736ca24018184b689adecec7329360f3e
Larry Ellison <93025344+Ellison98@users.noreply.github.com>
2024-07-04 11:00:58 +0900

    [level 0] Title: 문자열 섞기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,10 +1,3 @@
 function solution(str1, str2) {
-    let answer = []
-    
-    for (let i = 0; i < str1.length; i++) {
-        answer.push(str1[i])
-        answer.push(str2[i])
-    }
-    
-    return answer.join('')
+    return [...str1].map((v, idx) => v + str2[idx]).join('')
 }
