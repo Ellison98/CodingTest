5936389bb1dc2e72dbeed2db8c89b63a17b8582f
Larry Ellison <93025344+Ellison98@users.noreply.github.com>
2025-01-05 22:05:19 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,7 +1,13 @@
 function solution(n, k) {
-    if (n >= 10) {
-        k -= ~~(n/10)
-    }
-
-    return 12000 * n + 2000 * k;
+    
+    // n 1개의 가격은 12,000
+    let n_total = n * 12000
+    
+    // k 1개의 가격은 2,000
+    let k_total = k * 2000
+    
+    // n 의 서비스 가격 제거
+    let servicePrice = Math.floor(n / 10) * 2000
+    
+    return n_total + k_total - servicePrice
 }
