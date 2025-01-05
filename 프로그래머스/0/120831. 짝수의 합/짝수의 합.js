a458f6e59cbd0897fcbcbc80fb8eb069eaa8aa7e
Larry Ellison <93025344+Ellison98@users.noreply.github.com>
2025-01-05 21:56:08 +0900

    [level 0] Title: 짝수의 합, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,3 +1,12 @@
 function solution(n) {
-    return Array(n).fill().map((_, i) => i+1).filter(v => v % 2 === 0).reduce((a, c) => a + c, 0)
+    var answer = 0;
+    
+    // n이하의 짝수를 찾는 조건문
+    for (let i = 0; i <= n; i++) {
+       if (i % 2 === 0) {
+           answer += i;
+       }  
+    }
+    
+    return answer;
 }
